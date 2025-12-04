2d42b1528650cf8324384d4da88e1e0efbfa33ac
Reybier <rsoto@hazling.com>
2025-12-04 01:37:34 -0400

    Ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tus-rifas-api.vercel.app/api' // Reemplazar con tu URL de Vercel del backend
+  apiUrl: 'https://tus-rifas-api.vercel.app/api'
 };
